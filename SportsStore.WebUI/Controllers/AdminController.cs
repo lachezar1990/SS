a1fb33e86704cd5eec09015e44cb3c3014f598f3
lachezar1990 <lachezar1990@abv.bg>
2016-04-29 21:53:46 +0300

    added admin panel + removed unused project

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(x => x.ProductID == productId);
+            return View(product);
+        }
+    }
+}
